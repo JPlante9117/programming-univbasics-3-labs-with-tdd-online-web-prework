9635aea0a76e6e1907d079e8f201b9d0e50156bc
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-09-04 15:46:41 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,7 +4,7 @@ def calculator(first_number, operator, second_number)
     quotient = first_number / second_number
     product = first_number * second_number
     
-    return operator
+    puts operator
 end
 
-return calculator (4, sum, 5)
+calculator (4, sum, 5)
