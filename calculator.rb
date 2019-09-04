813842c8ed9ec405abd645a171065ac373df4737
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-09-04 15:43:32 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,6 @@
 def calculator(first_number, operator, second_number)
+    sum = first_number + second_number
+    difference = first_number - second_number
+    quotient = first_number * second_number
     
 end
